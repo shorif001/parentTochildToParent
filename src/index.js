e04d70781ec404140b9e31d3e133aa06a80f8fad
Shorif <shorifsimec@gmail.com>
2024-03-03 18:47:48 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    // 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
     <>
     <App />
     </>
